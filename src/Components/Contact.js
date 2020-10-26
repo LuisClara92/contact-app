45362986e6e139b0dd77b66287543664a0239b6e
LuisClara92 <lc.sbg.92@gmail.com>
2020-10-26 14:55:34 +0000

    create repo of contact-app

---
+++ src/Components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Contact.css'
+
+
+//Contact app should receives props to render the information 
+
+//Contact class will be applied, using className rather than class
+
+//Create a GitHub repo for the contact-app, push the project that you have made and use the GitHub repo url for your solution.
+
+
+
+export default Contact
