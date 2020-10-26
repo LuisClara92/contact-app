c79b760c646d5501d91b2a4da1406675b2e0edef
LuisClara92 <lc.sbg.92@gmail.com>
2020-10-26 16:08:01 +0000

    finish contact cart

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Contact from './Components/Contact'
 export default function App() {
   return (
     <div>
-      <Contact />
+      <Contact
+        nickName="Artha_Dragon"
+        avatar="https://miro.medium.com/max/500/1*xioaMvyk5Vr36Qr9g3e47g.jpeg"
+        status='true'
+      />
     </div>
   );
 }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import './Contact.css'
 
-
 //Contact app should receives props to render the information 
 
-//Contact class will be applied, using className rather than class
+export default function Contact(props) {
+  return (
 
-//Create a GitHub repo for the contact-app, push the project that you have made and use the GitHub repo url for your solution.
-
-
-
-export default Contact
+    <div className="Contact">
+      <div className={props.status ? "shadow-online" : "shadow-offline"}>
+        <img className="avatar" src={props.avatar} alt="" />
+        <div>
+          <h4 className="name">{props.nickName}</h4>
+          <div className="status">
+            <div className={props.status ? "status-online" : "status-offline"}></div>
+            <div
+              className={
+                props.status ? "status-color-online" : "status-color-offline"
+              }
+            ></div>
+            <p className="status-text"> {props.status ? "Online" : "Offline"} </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
